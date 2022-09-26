b077666aa56df6aabad0c8b71f2d75e537f6769b
TheRealM4rtin <dinamyx.ytb@gmail.com>
2022-09-26 16:40:11 +0300

    working on User page

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import tkinter
 import customtkinter as ctk
-from pages import *
+import pages as pages
 
-# Create a button
-def button_function():
-    print("button pressed")
 
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
@@ -12,32 +9,25 @@ class App(ctk.CTk):
 
         ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
         ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
         self.geometry("400x400")
         self.title("Home Page")
-
 
         container = ctk.CTkFrame(self)
         container.grid(row=0, column=0, sticky="nsew")
 
         self.id = ctk.StringVar()
         self.id.set("welcome_page")
-
         self.frames = {}  # Dictionary of frames
 
-        # welcome_page = welcome_page(container, self)
-        # admin_page = admin_page(container, self)
-        # user_page = user_page(container, self)
+        # List of frames
+        self.p = [pages.welcome_page, pages.admin_page, pages.user_page]
 
-        self.pages = [welcome_page, admin_page, user_page] # List of frames
-
-        for F in self.pages:
+        for F in self.p:
             frame = F(parent=container, controller=self)  # parent, controller
             frame.grid(row=0, column=0, sticky="nsew")
             self.frames[F] = frame
 
-        self.up_frame(welcome_page)
-
+        self.up_frame(pages.welcome_page)
     def up_frame(self, page_name):
         page = self.frames[page_name]
         page.tkraise()
--- pages.py
+++ pages.py
@@ -1,46 +1,49 @@
 import tkinter
 import customtkinter as ctk
+import csv
 
-import framesconfig
-from buttons import QuitButton
-from framesconfig import *
 
 class admin_page(ctk.CTkFrame):
     def __init__(self, parent, controller):
         ctk.CTkFrame.__init__(self, parent)
 
         # Horizontal Frames
-        # self.frame_top = ctk.CTkFrame(self, bg="blue")
-        # self.frame_middle = ctk.CTkFrame(self, bg="blue")
+        self.frame_top = ctk.CTkFrame(self, bg="blue")
+        self.frame_middle = ctk.CTkFrame(self, bg="blue")
+        self.frame_bottom = ctk.CTkFrame(self, bg="blue")
+        # Grid Frames
+        self.frame_top.grid(row=0, column=0, sticky="nsew")
+        self.frame_middle.grid(row=1, column=0, sticky="nsew")
+        self.frame_bottom.grid(row=2, column=0, sticky="nsew")
+        # Column config
+        self.frame_top.columnconfigure(0, weight=1)
+        self.frame_middle.columnconfigure(0, weight=1)
+        self.frame_middle.columnconfigure(1, weight=1)
+        self.frame_middle.columnconfigure(2, weight=1)
+        self.frame_bottom.columnconfigure(0, weight=1)
+        # Row Config
+        self.frame_top.rowconfigure(0, weight=1)
+        self.frame_middle.rowconfigure(0, weight=1)
+        self.frame_middle.rowconfigure(1, weight=1)
+        self.frame_middle.rowconfigure(2, weight=1)
+        self.frame_bottom.rowconfigure(0, weight=1)
+
+        # Text
+        self.string_header = tkinter.StringVar(value="Welcome Admin!")
+        self.label_header = ctk.CTkLabel(master=self.frame_top, textvariable=self.string_header, width=120,
+                                         height=25)
+        self.label_header.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+
+        # def delete_data():
         #
 
-        # Grid Frames
-        # self.frame_top.grid(row=0, column=0, sticky="nsew")
-
-        frametop = framesconfig.frame_top( 0, 0, "nsew")
-        framemiddle = framesconfig.frame_middle( 1, 0, "nsew")
-
-        self.framemiddle.grid(row=1, column=0, sticky="nsew")
-        # self.frame_bottom.grid(row=2, column=0, sticky="nsew")
-        # Column config
-        self.frametop.columnconfigure(0, weight=1)
-        self.framemiddle.columnconfigure(0, weight=1)
-        self.framemiddle.columnconfigure(1, weight=1)
-        self.framemiddle.columnconfigure(2, weight=1)
-        # Row Config
-        self.frametop.rowconfigure(0, weight=1)
-        self.framemiddle.rowconfigure(0, weight=1)
-        self.framemiddle.rowconfigure(1, weight=1)
-        self.framemiddle.rowconfigure(2, weight=1)
-
-        # Text admin
-        self.string_wlcm = tkinter.StringVar(value="Welcome A D M I N")
-        self.label_wlcm = ctk.CTkLabel(master=self.frametop, textvariable=self.string_wlcm, width=120,
-                                                  height=25)
-        self.label_wlcm.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
         # Quit Button
-        self.quit_button = QuitButton(self.framemiddle, controller)
+        self.button_quit = ctk.CTkButton(master=self.frame_bottom, text="Quit",
+                                         command=lambda: controller.up_frame(welcome_page))
+        self.button_quit.grid(row=2, column=1, padx=10, pady=10)
+
 
 class user_page(ctk.CTkFrame):
     def __init__(self, parent, controller):
@@ -49,28 +52,76 @@ class user_page(ctk.CTkFrame):
         # Horizontal Frames
         self.frame_top = ctk.CTkFrame(self, bg="blue")
         self.frame_middle = ctk.CTkFrame(self, bg="blue")
-        # self.frame_bottom = customtkinter.CTkFrame(self, bg="blue")
+        self.frame_bottom = ctk.CTkFrame(self, bg="blue")
         # Grid Frames
         self.frame_top.grid(row=0, column=0, sticky="nsew")
         self.frame_middle.grid(row=1, column=0, sticky="nsew")
-        # self.frame_bottom.grid(row=2, column=0, sticky="nsew")
+        self.frame_bottom.grid(row=2, column=0, sticky="nsew")
         # Column config
         self.frame_top.columnconfigure(0, weight=1)
         self.frame_middle.columnconfigure(0, weight=1)
         self.frame_middle.columnconfigure(1, weight=1)
         self.frame_middle.columnconfigure(2, weight=1)
+        self.frame_bottom.columnconfigure(0, weight=1)
         # Row Config
         self.frame_top.rowconfigure(0, weight=1)
         self.frame_middle.rowconfigure(0, weight=1)
         self.frame_middle.rowconfigure(1, weight=1)
         self.frame_middle.rowconfigure(2, weight=1)
+        self.frame_middle.rowconfigure(3, weight=1)
+        self.frame_bottom.rowconfigure(0, weight=1)
 
-        # Text admin
-        self.string_wlcm = tkinter.StringVar(value="Welcome User")
+        # Text
+        self.string_wlcm = tkinter.StringVar(value="Welcome Student!")
         self.label_wlcm = ctk.CTkLabel(master=self.frame_top, textvariable=self.string_wlcm, width=120,
-                                                  height=25)
+                                       height=25)
         self.label_wlcm.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
+        # Entry Field
+        entry = ctk.CTkEntry(master=self.frame_middle,
+                             placeholder_text="Name",
+                             width=120,
+                             height=25,
+                             border_width=2,
+                             corner_radius=10)
+        entry.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+
+        # Option Box
+        status_var = ctk.StringVar(value="Present")  # set initial value
+
+        combobox = ctk.CTkComboBox(master=self.frame_middle,
+                                   values=["Present", "Late", "Absent"],
+                                   variable=status_var)
+        combobox.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+
+
+        # Check Box
+        checkbox_var = tkinter.StringVar(self.frame_middle, "Disagree")
+
+        checkbox = ctk.CTkCheckBox(master=self.frame_middle,
+                                   text="I certify on my honour that the above information are "
+                                        "correct.",
+                                   variable=checkbox_var, onvalue="Agree", offvalue="Disagree")
+        checkbox.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        def save_data():
+            f = open('data.csv', 'a', newline='')
+            var = (entry.get(), status_var.get(), checkbox_var.get())
+            writer = csv.writer(f)
+            writer.writerow(var)
+            print("SUBMITTED")
+            f.close()
+
+        # Submit Button
+        self.button_submit = ctk.CTkButton(master=self.frame_bottom, text="Submit", command=save_data)
+
+        self.button_submit.grid(row=2, column=0, padx=10, pady=10)
+
+        # Quit Button
+        self.button_quit = ctk.CTkButton(master=self.frame_bottom, text="Quit",
+                                         command=lambda: controller.up_frame(welcome_page))
+        self.button_quit.grid(row=2, column=1, padx=10, pady=10)
 
 
 class welcome_page(ctk.CTkFrame):
@@ -82,29 +133,36 @@ class welcome_page(ctk.CTkFrame):
         # Horizontal Frames
         self.frame_top = ctk.CTkFrame(self, bg="blue")
         self.frame_middle = ctk.CTkFrame(self, bg="blue")
-        # self.frame_bottom = customtkinter.CTkFrame(self, bg="blue")
+        self.frame_bottom = ctk.CTkFrame(self, bg="blue")
         # Grid Frames
         self.frame_top.grid(row=0, column=0, sticky="nsew")
         self.frame_middle.grid(row=1, column=0, sticky="nsew")
-        # self.frame_bottom.grid(row=2, column=0, sticky="nsew")
+        self.frame_bottom.grid(row=2, column=0, sticky="nsew")
         # Column config
         self.frame_top.columnconfigure(0, weight=1)
         self.frame_middle.columnconfigure(0, weight=1)
         self.frame_middle.columnconfigure(1, weight=1)
         self.frame_middle.columnconfigure(2, weight=1)
+        self.frame_bottom.columnconfigure(0, weight=1)
         # Row Config
         self.frame_top.rowconfigure(0, weight=1)
         self.frame_middle.rowconfigure(0, weight=1)
         self.frame_middle.rowconfigure(1, weight=1)
         self.frame_middle.rowconfigure(2, weight=1)
+        self.frame_bottom.rowconfigure(0, weight=1)
 
         # Text Welcome
         self.string_wlcm = tkinter.StringVar(value="Welcome to the Assessment Management System")
         self.label_wlcm = ctk.CTkLabel(master=self.frame_top, textvariable=self.string_wlcm, width=120,
-                                                  height=25)
+                                       height=25)
         self.label_wlcm.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
-        # Button User
-        self.button_user = ctk.CTkButton(master=self.frame_middle, text="User", command=lambda: controller.up_frame(user_page))
-        self.button_user.grid(row=1, column=1, padx=10, pady=10)
-        # Button Admin
 
+        # Button User
+        self.button_user = ctk.CTkButton(master=self.frame_middle, text="User",
+                                         command=lambda: controller.up_frame(user_page))
+        self.button_user.grid(row=1, column=1, padx=10, pady=10)
+
+        # Button Admin
+        self.button_admin = ctk.CTkButton(master=self.frame_middle, text="Admin",
+                                          command=lambda: controller.up_frame(admin_page))
+        self.button_admin.grid(row=2, column=1, padx=10, pady=10)
